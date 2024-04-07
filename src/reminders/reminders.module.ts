9a62bc205fa221362daa157ad9dc1f7a2cd11e5c
LuccaHaddadSerejo <hslucca@gmail.com>
2024-04-06 21:39:51 -0300

    feature (reminders-module): Added prisma service and repository to reminders providers

--- src/reminders/reminders.module.ts
+++ src/reminders/reminders.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RemindersService } from './reminders.service';
 import { RemindersController } from './reminders.controller';
+import { PrismaService } from 'src/database/prisma.service';
+import { RemindersRepository } from './reminders.repository';
 
 @Module({
   controllers: [RemindersController],
-  providers: [RemindersService],
+  providers: [RemindersService, PrismaService, RemindersRepository],
 })
 export class RemindersModule {}
