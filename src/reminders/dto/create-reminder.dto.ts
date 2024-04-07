e527cfb67884b2a0547d123272aa6364625ddc3d
LuccaHaddadSerejo <hslucca@gmail.com>
2024-04-06 21:07:08 -0300

    feature (reminders-DTO): Created reminder DTO class

--- src/reminders/dto/create-reminder.dto.ts
+++ src/reminders/dto/create-reminder.dto.ts
@@ -1,1 +1,30 @@
-export class CreateReminderDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsDate,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateReminderDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(30, { message: 'The title can only have up to 30 characters' })
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsDate()
+  date: Date;
+
+  @IsNotEmpty()
+  @IsString()
+  city: string;
+
+  @IsNotEmpty()
+  @IsString()
+  color: string;
+}
