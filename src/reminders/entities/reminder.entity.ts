0012748d0c68db2298958ed9ec3b0a1a7f3d1d3e
LuccaHaddadSerejo <hslucca@gmail.com>
2024-04-06 21:06:44 -0300

    feature (reminders-entity): Created reminder entity class

--- src/reminders/entities/reminder.entity.ts
+++ src/reminders/entities/reminder.entity.ts
@@ -1,1 +1,10 @@
-export class Reminder {}
+export class Reminder {
+  readonly id: number;
+  title: string;
+  description?: string;
+  date: Date;
+  city: string;
+  color: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
